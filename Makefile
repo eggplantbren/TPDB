0b766599a380c1b91f55350ea9bb1e274bbc59ff
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-03-26 19:50:06 +1300

    Some starting stuff

---
+++ Makefile
@@ -0,0 +1,12 @@
+CXX = g++
+FLAGS = -std=c++17
+INCLUDE = -I . -I$(DNEST4_PATH)
+OPTIM = -O2 -g
+WARN = -Wall -Wextra -pedantic
+ALL = $(FLAGS) $(INCLUDE) $(OPTIM) $(WARN)
+
+default:
+	$(CXX) $(ALL) -c TPDB/Example.hpp
+	$(CXX) $(ALL) -c main.cpp
+	$(CXX) -L$(DNEST4_PATH)/DNest4/code -o main main.o -ldnest4
+	rm -f main.o TPDB/*.gch
---
+++ TPDB/Example.hpp
@@ -0,0 +1,85 @@
+#ifndef TPDB_Example_hpp
+#define TPDB_Example_hpp
+
+#include "DNest4/code/RNG.h"
+#include "DNest4/code/Utils.h"
+#include <tuple>
+
+using DNest4::RNG;
+using DNest4::wrap;
+
+namespace TPDB
+{
+
+/*
+* A simple example model.
+*/
+class Example
+{
+    private:
+        static constexpr int N = 20; // Dimensionality
+        std::vector<double> xs;
+
+    public:
+
+        // Constructor
+        Example();
+
+        // Generate from prior
+        void from_prior(RNG& rng);
+
+        // Metropolis proposal in-place, return log(hastings) for prior sampling
+        double perturb(RNG& rng);
+
+        // Return scalars
+        std::tuple<double, double> get_scalars() const;
+
+        // Render as string
+        std::string render() const;
+
+};
+
+/* IMPLEMENTATIONS FOLLOW */
+
+Example::Example()
+:xs(N)
+{
+
+}
+
+void Example::from_prior(RNG& rng)
+{
+    for(int i=0; i<N; ++i)
+        xs[i] = rng.rand();
+}
+
+double Example::perturb(RNG& rng)
+{
+    int k = rng.rand_int(N);
+    xs[k] += rng.randh();
+    wrap(xs[k], 0.0, 1.0);
+    return 0;
+}
+
+std::tuple<double, double> Example::get_scalars() const
+{
+    double f1 = 0.0;
+    double f2 = 0.0;
+
+    // Parameters of the two peaks
+    static constexpr double w = 0.01;
+    static constexpr double tau = 1.0/(w*w);
+
+    for(int i=0; i<N; ++i)
+    {
+        f1 += -0.5*tau*pow(xs[i] - 0.4, 2);
+        f2 += -0.5*tau*pow(xs[i] - 0.6, 2);
+    }
+
+    return {f1, f2};
+}
+
+} // namespace
+
+#endif
+
